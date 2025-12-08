57ce57f7fe2f4f41a9fe6b3146575cab133e02da
HokageObito <timur.gorlachyov@yandex.ru>
2025-12-08 21:17:54 +0300

    add new script followCam

---
+++ Assets/Code/PlayerAndCamMove/followCam.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class followCam : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    public float sensetivity = 250f;
+    public float xRotation = 0;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    public void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensetivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensetivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -75f, 75f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        player.Rotate(Vector3.up * mouseX);
+    }
+
+}
