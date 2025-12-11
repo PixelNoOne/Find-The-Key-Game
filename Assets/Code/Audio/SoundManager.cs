ebf33682c08fbd0722d8d132a96dc548e1acbbfd
HokageObito <timur.gorlachyov@yandex.ru>
2025-12-11 06:07:18 +0300

    update Audio script

--- Assets/Code/Audio/SoundManager.cs
+++ Assets/Code/Audio/SoundManager.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioSource steps;
+    [SerializeField] private AudioSource vectorOpenSound;
+    [SerializeField] private AudioSource eulerOpenSound;
     void Start()
     {
         audioSource.Play();
